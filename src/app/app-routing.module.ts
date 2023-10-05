02bb0397f8e0c5e1f8fa02692091b93372189c6a
kaguan <karen.guan@student.csulb.edu>
2023-10-04 23:18:39 -0700

    html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChatpageComponent } from './chatpage/chatpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'chatpage',  component :ChatpageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
